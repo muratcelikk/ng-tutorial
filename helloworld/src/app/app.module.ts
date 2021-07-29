9e189a29a203015bfe3e344f79335ea71901fc71
Murat Çelik <muratcelik11@gmail.com>
2021-07-29 03:18:32 +0300

    via

--- helloworld/src/app/app.module.ts
+++ helloworld/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TextcomponentComponent } from './textcomponent/textcomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextcomponentComponent
   ],
   imports: [
     BrowserModule,
